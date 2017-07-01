fb3202875ae8466c7f22b8c24415a4b840b65856
Hendi Santika <hendisantika@gmail.com>
2017-07-01 09:32:40 +0700

    #7 - Get File Name in Kotlin

--- src/main/kotlin/com/hendisantika/oprek/campuran/FileName.kt
+++ src/main/kotlin/com/hendisantika/oprek/campuran/FileName.kt
@@ -3,3 +3,15 @@ package com.hendisantika.oprek.campuran
 /**
  * Created by hendisantika on 7/1/17.
  */
+fun main(args: Array<String>) {
+
+    val fullPath = "Kotlination/Kotlin/Practice/getFileNameExample.kt"
+
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(fullPath)
+
+    if (matchResult != null) {
+        val (directory, fileName, extension) = matchResult.destructured
+        println("dir: $directory | fileName: $fileName | extension: $extension")
+    }
+}
