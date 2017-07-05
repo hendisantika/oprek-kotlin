20d128f4b75c6e0a7e8ad09ffee47edf3b81bffc
Hendi Santika <hendisantika@gmail.com>
2017-07-05 13:36:22 +0700

    #18 - Added Customer POJO

---
+++ src/main/java/com/hendisantika/java/Customer.java
@@ -0,0 +1,25 @@
+package com.hendisantika.java;
+
+/**
+ * Created by hendisantika on 7/5/17.
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/kotlin/com/hendisantika/oprek/test/ArrayTest.kt
@@ -0,0 +1,7 @@
+package com.hendisantika.oprek.test
+
+/**
+ * Created by hendisantika on 7/5/17.
+ */
+class ArrayTest {
+}
