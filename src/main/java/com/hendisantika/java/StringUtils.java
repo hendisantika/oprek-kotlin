6e5c4526c2bc9121dad045d6d0089801f4e07ade
Hendi Santika <hendisantika@gmail.com>
2017-07-06 09:47:27 +0700

    #21 - Added StringUtils

---
+++ src/main/java/com/hendisantika/java/StringUtils.java
@@ -0,0 +1,10 @@
+package com.hendisantika.java;
+
+/**
+ * Created by hendisantika on 7/6/17.
+ */
+public class StringUtils {
+    public static String toUpperCase(String name) {
+        return name.toUpperCase();
+    }
+}
