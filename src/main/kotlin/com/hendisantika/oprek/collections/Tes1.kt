310e687986c1f1a4efd5ff8954686d703cf3f07b
Hendi Santika <hendisantika@gmail.com>
2018-11-06 06:48:44 +0700

    #34 - Upgraded into Kotlin 1.3.0 version

---
+++ src/main/kotlin/com/hendisantika/oprek/collections/Tes1.kt
@@ -0,0 +1,15 @@
+package com.hendisantika.oprek.collections
+
+/**
+ * Created by hendisantika on 7/13/17.
+ */
+fun main(args: Array<String>) {
+    val map = mapOf(
+            "success" to true,
+            "friends" to listOf("aku", "dia", "mereka"),
+            "count" to 1
+
+    )
+
+    println("Data map --> $map")
+}
